32e9f7dbe274c71e0937cfae936501c0d9d4df64
Ján Jurč <jurcik46@gmail.com>
2018-11-17 02:13:48 +0100

    last

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 #include "logging/log.h"
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #define DELAY 30000
 
 void nacitajMapu(){
@@ -46,7 +51,14 @@ char** createM(int m, int n) {
 
     }
 
-int main() {
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+
+int main(int argc, char *argv[]) {
 
     log_set_quiet(0);
     log_set_level(LOG_DEBUG);
@@ -58,6 +70,45 @@ int main() {
         // Program exits if the file pointer returns NULL.
         return 1;
     }
+
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[256];
+//    if (argc < 3) {
+//        fprintf(stderr,"usage %s hostname port\n", argv[0]);
+//        exit(0);
+//    }
+    portno = atoi("2666");
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    server = gethostbyname("localhost");
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,
+          (char *)&serv_addr.sin_addr.s_addr,
+          server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+        error("ERROR connecting");
+    printf("Please enter the message: ");
+    bzero(buffer,256);
+    fgets(buffer,255,stdin);
+    n = write(sockfd,buffer,strlen(buffer));
+    if (n < 0)
+        error("ERROR writing to socket");
+    bzero(buffer,256);
+    n = read(sockfd,buffer,255);
+    if (n < 0)
+        error("ERROR reading from socket");
+    printf("%s\n",buffer);
+    close(sockfd);
 
 //    log_trace(const char *fmt, ...);
 //    log_debug(const char *fmt, ...);
