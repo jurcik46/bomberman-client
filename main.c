97712a0fcd41703895eacfad6f047b236dd1905d
Ján Jurč <jurcik46@gmail.com>
2018-11-21 20:19:10 +0100

    cleaned main

--- main.c
+++ main.c
@@ -12,105 +12,31 @@
 #include "logging/log.h"
 #include "communication.h"
 
-#define DELAY 30000
 
-void nacitajMapu() {
-    FILE *vstup;
-    if ((vstup = fopen("mapa.txt", "r")) == NULL) {
-        int riadok, stlpec;
-//    while(){};
+#define LOG_FILE_PATH "logs.log"
+#define PORT 8080
 
-    }
-
-}
-
-/**
- * alokovanie pamete pre mapu
- * @param m riadok
- * @param n stlpec
- * @return smernik dvojrozmerne pole
- */
-char **createM(int m, int n) {
-    char **mat;
-    mat = malloc(m * sizeof(*mat));
-    for (int i = 0; i < m; i++) {
-        mat[i] = calloc(n, sizeof(char));
-    }
-    return mat;
-}
-
-/**
- * Uvolnenie pamete pre mapu
- * @param m
- * @param mat
- */
-
-void deletM(int m, char ***mat) {
-    for (int i = 0; i < m; i++) {
-        free((*mat)[i]);
-    }
-    free((*mat));
-    *mat = NULL;
-
-}
-
-void error(const char *msg) {
-    perror(msg);
-    exit(0);
-}
-
-
-int main(int argc, char *argv[]) {
-
-    log_set_quiet(0);
-    log_set_level(LOG_DEBUG);
-    FILE *logFile = fopen("logs.log", "w+");
-    log_set_fp(logFile);
+void loggerInit(FILE *logFile) {
 
     if (logFile == NULL) {
         printf("Error! opening file");
         // Program exits if the file pointer returns NULL.
-        return 1;
+        exit(EXIT_FAILURE);
     }
-//
-//    int sockfd, portno, n;
-//    struct sockaddr_in serv_addr;
-//    struct hostent *server;
-//
-//    char buffer[256];
-////    if (argc < 3) {
-////        fprintf(stderr,"usage %s hostname port\n", argv[0]);
-////        exit(0);
-////    }
-//    portno = atoi("2666");
-//    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-//    if (sockfd < 0)
-//        error("ERROR opening socket");
-//    server = gethostbyname("localhost");
-//    if (server == NULL) {
-//        fprintf(stderr, "ERROR, no such host\n");
-//        exit(0);
-//    }
-//    bzero((char *) &serv_addr, sizeof(serv_addr));
-//    serv_addr.sin_family = AF_INET;
-//    bcopy((char *) server->h_addr,
-//          (char *) &serv_addr.sin_addr.s_addr,
-//          server->h_length);
-//    serv_addr.sin_port = htons(portno);
-//    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-//        error("ERROR connecting");
-//    printf("Please enter the message: ");
-//    bzero(buffer, 256);
-//    fgets(buffer, 255, stdin);
-//    n = write(sockfd, buffer, strlen(buffer));
-//    if (n < 0)
-//        error("ERROR writing to socket");
-//    bzero(buffer, 256);
-//    n = read(sockfd, buffer, 255);
-//    if (n < 0)
-//        error("ERROR reading from socket");
-//    printf("%s\n", buffer);
-//    close(sockfd);
+    log_set_quiet(0);
+    log_set_level(LOG_DEBUG);
+    log_set_fp(logFile);
+}
+
+void closingApp(FILE *logFile) {
+    closeSocket();
+    fclose(logFile);
+};
+
+int main(int argc, char *argv[]) {
+
+    FILE *logFile = fopen(LOG_FILE_PATH, "w+");
+    loggerInit(logFile);
 
     initSocket("127.0.0.1", 8080);
     char *hello = "Hello from client";
@@ -118,8 +44,9 @@ int main(int argc, char *argv[]) {
     printf("Hello message sent\n");
     read(sock, buffer, BUFFER_SIZE);
     printf("%s\n", buffer);
+    sleep(10);
 
-    fclose(logFile);
+    closingApp(logFile);
     return 0;
 
 
