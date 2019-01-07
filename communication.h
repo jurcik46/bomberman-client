d98095f4618dab9cc7fe89a17b258591a1cab77c
jurcik46 <jurcik46@gmail.com>
2019-01-07 18:46:07 +0100

    create game communication and function for checking if socket is ready for read write

--- communication.h
+++ communication.h
@@ -10,24 +10,49 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <errno.h>
 
 #include "logging/log.h"
 #include "constants.h"
 
 #define BUFFER_SIZE 1024
+#define NAME_LENGTH 50
 
+typedef struct socket {
+    int sock;
+    int stop;
+
+    char buffer[BUFFER_SIZE];
+    _Bool end;
+    pthread_mutex_t mutex;
+} Socket;
+
+pthread_t socketThread;
+/**
+ * Communication Select
+ */
+fd_set socketDs;
+int sd;
+int activity;
 
 struct sockaddr_in address;
-int sock;
 struct sockaddr_in serv_addr;
-char buffer[BUFFER_SIZE];
 
+Socket sock;
 
 enum result_code communication(enum communication_type commuType, char *data);
 
 enum result_code loginToServer(char *data);
 
+enum result_code createGameToServer(char *data);
+
 void initSocket(char *ipAddress, u_int16_t port);
+
+_Bool socketReady();
+
+void initThreadSocket();
 
 void closeSocket();
 
